cefc5dceb5642b37159aaf35c507df88536c4bfe
xiaoguokf <1742137309@qq.com>
2023-02-16 22:34:40 +0800

    proxy

--- api/proxy.js
+++ api/proxy.js
@@ -6,6 +6,7 @@ module.exports = (req, res) => {
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
     if (req.url.startsWith('/api')) {
         target = 'http://106.52.150.170:8080/'
+        console.log("检测");
     }
 
     // 创建代理对象并转发请求
